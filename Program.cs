319561782f267515b8ed66040b34abc4a9c3d572
sneg <sneg.sbg@gmail.com>
2023-01-21 11:51:28 +0300

    dooble_number working

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int num = number /10;
+int num2 = num % 10;
+
+Console.WriteLine($"Вторая цифра {number} это {num2}");
